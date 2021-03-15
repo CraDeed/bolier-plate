07034ca6ef2def70b9cfbbe7c79fd34e4f0a0a2f
CraDeed <56147655+CraDeed@users.noreply.github.com>
2021-03-15 12:44:44 +0900

    Init Respo

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+const mongoose = require("mongoose");
+mongoose
+    .connect(
+        "mongodb+srv://dldmsrll1075:dldmsrl456@bolierplate.q2qen.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+        }
+    )
+    .then(() => console.log("MongoDB Connected..."))
+    .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+    res.send("Hello World! 안녕하떼유!");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
